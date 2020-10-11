daceb79a1c560f7ee64aff5fed04a4efd17670e0
Miluckshan_V_J <52714282+miluckshan-j@users.noreply.github.com>
2020-10-11 16:05:36 +0530

    first commit

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public Transform target = null;
+
+    public Transform player = null;
+
+    public Vector3 newposition = Vector3.zero;
+    public float speed = 5.0f;
+
+    void LateUpdate()
+    {
+
+        newposition.x = Mathf.Lerp(this.transform.position.x, target.position.x, speed * Time.deltaTime);
+        newposition.y = Mathf.Lerp(this.transform.position.y, target.position.y+1, speed * Time.deltaTime);
+        newposition.z = Mathf.Lerp(this.transform.position.z, target.position.z, speed * Time.deltaTime);
+
+        this.transform.position = newposition;
+
+        this.transform.LookAt(player.position);
+
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float movespeed = 0.5f;
+    public float rotatespeed = 100.0f;
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        if (anim != null)
+        {
+            anim.Play("Idle", 0);
+            Vector3 spawn = new Vector3(-5.02f, 0.05f, 8.13f);
+            this.gameObject.transform.position = spawn;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (anim != null)
+            {
+                anim.Play("Walking", 0);
+            }
+            transform.Translate(0, 0, movespeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if (anim != null)
+            {
+                anim.Play("Walking", 0);
+            }
+            transform.Translate(0, 0, -movespeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (anim != null)
+            {
+                anim.Play("Left Turn", 0);
+            }
+            this.transform.RotateAround(this.transform.position, transform.up, Time.deltaTime * -rotatespeed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (anim != null)
+            {
+                anim.Play("Right Turn", 0);
+            }
+            this.transform.RotateAround(this.transform.position, transform.up, Time.deltaTime * rotatespeed);
+
+        }
+    }
+}
